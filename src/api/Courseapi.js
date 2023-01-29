1a2ec52e11f0149d684188f8ada9f062c72ad763
indresh149 <indreshgoswami149@gmail.com>
2023-01-29 14:59:46 +0530

    first commit

---
+++ src/api/Courseapi.js
@@ -0,0 +1,70 @@
+const Courses = [
+  {
+    id: "1",
+    title: "Web Development",
+    image: require("../../assets/course/web.jpg"),
+    description:
+      "Web development course containing html, css, javascript, react, node, express, mongodb, etc.",
+    course1: "html",
+    course2: "css",
+    course3: "javascript",
+    price: 5000,
+  },
+  {
+    id: "2",
+    title: "Graphic Designing ",
+    image: require("../../assets/course/graphic.jpg"),
+    description:
+      "Graphic designing course containing photoshop, figma, adobe xd, etc. ",
+    course1: "Photoshop",
+    course2: "Figma",
+    course3: "Adobe XD",
+    price: 3000,
+  },
+  {
+    id: "3",
+    title: "Coding Fundamentals",
+    image: require("../../assets/course/coding.jpg"),
+    description:
+      "Coding fundamentals course containing javascript, algorithm, data structure, etc. ",
+    course1: "JavaScript",
+    course2: "Algorithm",
+    course3: "Data Structure",
+    price: 1000,
+  },
+  {
+    id: "4",
+    title: "Ethical Hacking",
+    image: require("../../assets/course/eth.jpg"),
+    description:
+      "Ethical hacking course containing testing, exploitation, linux os, etc. ",
+    course1: "Testing",
+    course2: "Exploitation",
+    course3: "Linux OS",
+    price: 6000,
+  },
+  {
+    id: "5",
+    title: "Web Development",
+    image: require("../../assets/course/web.jpg"),
+    description:
+      " Web development course containing html, css, javascript, react, node, express, mongodb, etc. ",
+    course1: "html",
+    course2: "css",
+    course3: "javascript",
+    price: 5000,
+  },
+  {
+    id: "6",
+    title: "Web Development",
+    image: require("../../assets/course/web.jpg"),
+    description:
+      " Web development course containing html, css, javascript, react, node, express, mongodb, etc. ",
+    course1: "html",
+    course2: "css",
+    course3: "javascript",
+    price: 5000,
+  },
+];
+
+export default Courses;
